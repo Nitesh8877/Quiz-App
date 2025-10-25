67f4608ee5245f354875f3ad15f4c44164bfc0ce
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2025-10-25 15:35:33 +0530

    change ui

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -4,6 +4,7 @@ import { useGlobalContext } from './context'
 import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
+
 function App() {
   const {
     waiting,
@@ -14,16 +15,18 @@ function App() {
     nextQuestion,
     checkAnswer,
   } = useGlobalContext()
+  
   if (waiting) {
     return <SetupForm />
   }
+  
   if (loading) {
     return <Loading />
   }
  
   const { question, incorrect_answers, correct_answer } = questions[index]
-  console.log(questions[index],question,incorrect_answers,correct_answer)
-  // const answers = [...incorrect_answers, correct_answer]
+  
+  // Shuffle answers
   let answers = [...incorrect_answers]
   const tempIndex = Math.floor(Math.random() * 4)
   if (tempIndex === 3) {
@@ -32,20 +35,34 @@ function App() {
     answers.push(answers[tempIndex])
     answers[tempIndex] = correct_answer
   }
+  
   return (
     <main>
       <Modal />
       <section className='quiz'>
-        <p className='correct-answers'>
-          correct answers : {correct}/{index}
-        </p>
+        <div style={{ marginBottom: '2rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', gap: '1rem' }}>
+          <div style={{ 
+            background: 'rgba(99, 102, 241, 0.1)', 
+            padding: '0.75rem 1.5rem', 
+            borderRadius: '0.75rem',
+            border: '1px solid rgba(99, 102, 241, 0.2)',
+            color: '#818cf8',
+            fontWeight: '600'
+          }}>
+            Question {index + 1} of {questions.length}
+          </div>
+          <p className='correct-answers'>
+            Score: {correct}/{index}
+          </p>
+        </div>
+        
         <article className='container'>
-          <h2 dangerouslySetInnerHTML={{ __html:question}} />
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
           <div className='btn-container'>
-            {answers.map((answer, index) => {
+            {answers.map((answer, answerIndex) => {
               return (
                 <button
-                  key={index}
+                  key={answerIndex}
                   className='answer-btn'
                   onClick={() => checkAnswer(correct_answer === answer)}
                   dangerouslySetInnerHTML={{ __html: answer }}
@@ -54,8 +71,9 @@ function App() {
             })}
           </div>
         </article>
+        
         <button className='next-question' onClick={nextQuestion}>
-          next question
+          Next Question
         </button>
       </section>
     </main>
